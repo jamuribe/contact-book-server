bcfd94f057e383a857b4a723375ac5924158fa63
Jose A. Molina <josemouribe@gmail.com>
2021-10-27 12:56:50 +0200

    feat(controllers): all methods working and implemented with error handling

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,22 +1,64 @@
+import dbRequest from "../requestToDB/requestToDB.js";
+import getRandomUser from "../randomUserAPI/randomUserAPI.js";
 
-const getAllContacts = (req, res) => {
-  // get contacts from db
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await dbRequest.getAll();
+    res.send(contacts);
+    res.status(200);
+  } catch (error) {
+    res.status(500);
+    console.log('Error: 1 ', error);
+  }
 }
 
-const updateContact = (req, res) => {
-  // update one contact
+const updateContact = async (req, res) => {
+  try {
+    const contact = req.body;
+    await dbRequest.updateOne(contact);
+    res.send('Contact updated successfully.');
+    res.status(201);
+  } catch (error) {
+    res.status(500);
+    console.log('Error: 2 ', error);
+  }
 }
 
-const addContact = (req, res) => {
-  // adds one contact to list
+const addContact = async (req, res) => {
+  try {
+    const newContact = req.body;
+    await dbRequest.addOne(newContact);
+    res.send('New contact added successfully.');
+    res.status(201);
+  } catch (error) {
+    res.status(500);
+    console.log('Error: 3 ', error);
+  }
 }
 
-const addRandomContact = (req, res) => {
-  // adds one contact to list
+const addRandomContact = async (req, res) => {
+  try {
+    const id = req.body.id;
+    const contact = await getRandomUser(id);
+    await dbRequest.addOne(contact);
+    res.send('Random user successfully added');
+    res.status(200)
+  } catch (error) {
+    res.status(500);
+    console.log('Error: 4 ', error);
+  }
 }
 
-const deleteContact = (req, res) => {
-  // deletes contact from db
+const deleteContact = async (req, res) => {
+  try {
+    const contact = req.body;
+    await dbRequest.deleteOne(contact);
+    res.send('Contact deleted successfully.');
+    res.status(201);
+  } catch (error) {
+    res.status(500);
+    console.log('Error: 5 ', error);
+  }
 }
 
 export default { getAllContacts, updateContact, addContact, deleteContact, addRandomContact };
