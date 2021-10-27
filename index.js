4f635c4a249579b58d367267548feadb4393b28c
Jose A. Molina <josemouribe@gmail.com>
2021-10-27 10:15:23 +0200

    feat(index js): initializing cors and dotenv

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import router from './router.js';
+import 'dotenv/config.js';
+import cors from 'cors';
 
 const app = express();
+const port = process.env.PORT || 3002;
 
-app.use(router);
 app.use(express.json());
-
-const port = 3001;
+app.use(cors());
+app.use(router);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
