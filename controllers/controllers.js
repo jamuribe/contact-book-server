4b50e4ae83894d064ba42dd450d83b0b4df5b7af
Jose A. Molina <josemouribe@gmail.com>
2021-10-27 09:34:10 +0200

    feat(controllers): defining functions

---
+++ controllers/controllers.js
@@ -0,0 +1,22 @@
+
+const getAllContacts = (req, res) => {
+  // get contacts from db
+}
+
+const updateContact = (req, res) => {
+  // update one contact
+}
+
+const addContact = (req, res) => {
+  // adds one contact to list
+}
+
+const addRandomContact = (req, res) => {
+  // adds one contact to list
+}
+
+const deleteContact = (req, res) => {
+  // deletes contact from db
+}
+
+export default { getAllContacts, updateContact, addContact, deleteContact, addRandomContact };
